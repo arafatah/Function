a0ae2c8da589254aafba9f8d0ea69f0f1422f3b0
arafatah <arafathowlader456@gmail.com>
2024-10-14 11:37:26 +0600

    Closure function end here

--- script.js
+++ script.js
@@ -262,8 +262,8 @@ const Data2 = [1, 5, 3, 9, 6, 1];
 
 poll.displayResults.call({ answers: Data1 }, 'string');
 poll.displayResults.call({ answers: Data2 });
-*/
 
+////////////////////////////
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
@@ -303,4 +303,81 @@ const Data2 = [1, 5, 3, 9, 6, 1];
 poll.displayResults.call({ answers: Data2 });
 poll.displayResults.call({ answers: Data1 }, 'string');
 
-// poll.registerNewAnswer();
+// poll.registerNewAnswer();*/
+
+/*
+// Immediately invoked function expression (IIFE) - onetime function - Function that use once.
+//USAGES: Protect data. Private data collection function. Data privacy.
+// Normal function
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+//IIFE
+(function () {
+  console.log('This will never run again and again');
+})();
+
+//Arrow IIFE function
+(() => console.log('This will also never run again anymore'))();
+
+//Closure
+let f;
+
+const g = function () {
+  const a = 12;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 111;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+h();
+f();
+
+//Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(() => {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each will ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} second`);
+};
+
+boardPassengers(180, 1)
+*/
+
+/*
+Coding Challenge #2
+This is more of a thinking challenge than a coding challenge �
+Your tasks:
+1. Take the IIFE below and at the end of the function, attach an event listener that 
+changes the color of the selected h1 element ('header') to blue, each time 
+the body element is clicked. Do not select the h1 element again!
+2. And now explain to yourself (or someone around you) why this worked! Take all 
+the time you need. Think about when exactly the callback function is executed, 
+and what that means for the variables involved in this example.
+ 
+GOOD LUCK 
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+    console.log('Clicked');
+  });
+})();
